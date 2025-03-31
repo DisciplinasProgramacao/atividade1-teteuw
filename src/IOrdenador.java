6d95e8006f28dde42197b1d3938faadf6de4e932
Mateus <mateuaraujo01@gmail.com>
2025-03-31 10:56:01 -0300

    upload de arquivos da oficina

--- src/IOrdenador.java
+++ src/IOrdenador.java
@@ -1,6 +1,8 @@
-public interface IOrdenador< T extends Comparable<T>> {
+
+public interface IOrdenador<T extends Comparable<T>>{
+
     public T[] ordenar(T[] dados);
     public long getComparacoes();
-    public long getMovimentacoes();
+	public long getMovimentacoes();
     public double getTempoOrdenacao();
 }
--- src/InsertSort.java
+++ src/InsertSort.java
@@ -2,18 +2,19 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 
-
-public class Bubblesort<T extends Comparable<T>> implements IOrdenador<T>{
+public class InsertSort<T extends Comparable<T>> implements IOrdenador<T>{
 
 	private long comparacoes;
 	private long movimentacoes;
 	private LocalDateTime inicio;
 	private LocalDateTime termino;	
 	
-	public Bubblesort() {
+	public InsertSort() {
 		comparacoes = 0;
 		movimentacoes = 0;
 	}
+	
+	
 
 	@Override
 	public T[] ordenar(T[] dados) {
@@ -22,29 +23,29 @@ public class Bubblesort<T extends Comparable<T>> implements IOrdenador<T>{
 		
 		inicio = LocalDateTime.now();
 		
-		for (int posReferencia = tamanho - 1; posReferencia > 0; posReferencia--) {
-			int trocas = 0;
-			for (int posicao = 0; posicao < posReferencia; posicao++) {
-				comparacoes++;
-				if (dadosOrdenados[posicao].compareTo(dadosOrdenados[posicao+1]) > 0){
-					swap (posicao, posicao + 1, dadosOrdenados);
-					trocas++;
-				}
-			}
-			if(trocas == 0 )
-				posReferencia = 0;
+		for (int posReferencia = 1; posReferencia <= tamanho -1; posReferencia++) {
+			T valor = dadosOrdenados[posReferencia];
+            int j = posReferencia-1;
+            comparacoes++;
+            while(j >=0 && valor.compareTo(dadosOrdenados[j]) <0){
+                j--;
+                comparacoes++;
+            }
+                
+            copiarDados(j+1, posReferencia, dadosOrdenados);
+            dadosOrdenados[j+1] = valor;
+            
 		}	
 		termino = LocalDateTime.now();
 
 		return dadosOrdenados;
 	}
 	
-	private void swap(int i, int j, T[] vet) {
-		movimentacoes++;
-		
-		T temp = vet[i];
-	    vet[i] = vet[j];
-	    vet[j] = temp;
+	private void copiarDados(int inicio, int fim, T[] vet) {
+		for (int i = fim; i > inicio; i--) {
+            movimentacoes++;
+            vet[i] = vet[i-1];
+        }
 	}
 	
 	public long getComparacoes() {
@@ -58,4 +59,8 @@ public class Bubblesort<T extends Comparable<T>> implements IOrdenador<T>{
 	public double getTempoOrdenacao() {
 	    return  Duration.between(inicio, termino).toMillis();	    
 	}
+
+	
+
+	
 }
